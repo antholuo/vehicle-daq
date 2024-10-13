f0760e094e2a481f8cf97c50566bac4f446c3ef3
Anthony Luo <anthonyluo100@gmail.com>
2024-10-12 21:59:10 -0700

    Remove fake SPI CS line

--- sw/sensor_board/sensor_board_rev0/Core/Src/gpio.c
+++ sw/sensor_board/sensor_board_rev0/Core/Src/gpio.c
@@ -41,6 +41,7 @@
      PB0   ------> S_TIM3_CH3
      PB1   ------> S_TIM3_CH4
      PA15   ------> S_TIM2_CH1_ETR
+     PB3   ------> S_TIM2_CH2
      PB4   ------> S_TIM3_CH1
      PB5   ------> S_TIM3_CH2
 */
@@ -53,9 +54,6 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
-
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(SPI1_CS_GPIO_Port, SPI1_CS_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : PB0 PB1 PB4 PB5 */
   GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_4|GPIO_PIN_5;
@@ -73,12 +71,13 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Alternate = GPIO_AF2_TIM2;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PtPin */
-  GPIO_InitStruct.Pin = SPI1_CS_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  /*Configure GPIO pin : PB3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(SPI1_CS_GPIO_Port, &GPIO_InitStruct);
+  GPIO_InitStruct.Alternate = GPIO_AF2_TIM2;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
