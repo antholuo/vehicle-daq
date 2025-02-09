9561c382c9d63ee34400a4df1fdbb9115d148400
Anthony Luo <anthonyluo100@gmail.com>
2025-02-08 22:38:11 -0800

    Reading data using ASM330LHH library directly

--- sw/nucleo_testy/Core/Src/gpio.c
+++ sw/nucleo_testy/Core/Src/gpio.c
@@ -50,7 +50,7 @@ void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(SPI_CS_GPIO_Port, SPI_CS_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(SPI_CS_GPIO_Port, SPI_CS_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIO_LED_GPIO_Port, GPIO_LED_Pin, GPIO_PIN_RESET);
--- sw/nucleo_testy/Debug/Drivers/BSP/Components/asm330lhh/asm330lhh_reg.d
+++
@@ -1,4 +0,0 @@
-Drivers/BSP/Components/asm330lhh/asm330lhh_reg.o: \
- ../Drivers/BSP/Components/asm330lhh/asm330lhh_reg.c \
- ../Drivers/BSP/Components/asm330lhh/asm330lhh_reg.h
-../Drivers/BSP/Components/asm330lhh/asm330lhh_reg.h:
