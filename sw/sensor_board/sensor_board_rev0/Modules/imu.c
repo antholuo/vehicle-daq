0fb5aa5fc6f6e522c51463c385399adc4d22ae4b
Anthony Luo <anthonyluo100@gmail.com>
2025-02-22 16:14:45 -0800

    ASM330LHH PID Library works

--- sw/sensor_board/sensor_board_rev0/Modules/imu.c
+++ sw/sensor_board/sensor_board_rev0/Modules/imu.c
@@ -9,15 +9,20 @@
 #include "gpio.h"
 #include "spi.h"
 
+#include "asm330lhh_reg.h"
+
 extern SPI_HandleTypeDef hspi1;
 
 void run_imu_basic() {
 	uint8_t count = 0;
-	uint8_t tx[2];
-	uint8_t rx[2];
 
-	tx[0] = 0x00; // pad second half of tx with 0's
-	tx[1] = 0x0F | 0x80; // 0x0F is WHOAMI, pad with 80 to make r/w bit high at the start
+	// IMU DEFINES
+	stmdev_ctx_t dev_ctx;
+
+	dev_ctx.write_reg = platform_write;
+	dev_ctx.read_reg = platform_read;
+	dev_ctx.mdelay = platform_delay;
+	dev_ctx.handle = &hspi1;
 
 	while(1) {
 		  count++;
@@ -27,10 +32,10 @@ void run_imu_basic() {
 		  }
 		  HAL_GPIO_TogglePin(GPIO_LED1_GPIO_Port, GPIO_LED1_Pin);
 
-		  // Send SPI Transaction
-		  HAL_SPI_TransmitReceive(&hspi1, tx, rx, 1, 1000);
+		  // Use the ASM330LHH PID driver to get device ID
+		  asm330lhh_device_id_get(&dev_ctx, &whoamI);
 
-		  if (rx[0] == 0x6B) {
+		  if (whoamI == ASM330LHH_ID) {
 			  while(1) {
 				  HAL_GPIO_WritePin(GPIO_LED1_GPIO_Port, GPIO_LED1_Pin, GPIO_PIN_SET);
 				  HAL_GPIO_TogglePin(GPIO_LED2_GPIO_Port, GPIO_LED2_Pin);
@@ -39,4 +44,26 @@ void run_imu_basic() {
 		  }
 		  HAL_Delay(100);
 	}
+}
+
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len)
+{
+  HAL_SPI_Transmit(handle, &reg, 1, 1000);
+  HAL_SPI_Transmit(handle, (uint8_t*) bufp, len, 1000);
+  return 0;
+}
+
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len)
+{
+  reg |= 0x80;
+  HAL_SPI_Transmit(handle, &reg, 1, 1000);
+  HAL_SPI_Receive(handle, bufp, len, 1000);
+  return 0;
+}
+
+static void platform_delay(uint32_t ms)
+{
+  HAL_Delay(ms);
 }
--- sw/sensor_board/sensor_board_rev0/Modules/imu.h
+++ sw/sensor_board/sensor_board_rev0/Modules/imu.h
@@ -8,6 +8,27 @@
 #ifndef IMU_H_
 #define IMU_H_
 
+#include <stdint.h>
+
+#include "asm330lhh_reg.h"
+
+typedef union {
+  int16_t i16bit[3];
+  uint8_t u8bit[6];
+} axis3bit16_t;
+
+static axis3bit16_t data_raw_acceleration;
+static axis3bit16_t data_raw_angular_rate;
+static axis3bit16_t dummy;
+static uint8_t whoamI, rst;
+
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len);
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len);
+//static void tx_com( uint8_t *tx_buffer, uint16_t len );
+static void platform_delay(uint32_t ms);
+
 void run_imu_basic();
 
 #endif /* IMU_H_ */
