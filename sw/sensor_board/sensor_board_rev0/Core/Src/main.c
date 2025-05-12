4a548a0960d7c650a0ff4868f121fd7addba23c4
Anthony Luo <anthonyluo100@gmail.com>
2025-05-12 09:28:22 -0400

    Add USART DMA in SB0 IOC
    Ch3. Periph->memory, normal mode low priority. Increment mem.

--- sw/sensor_board/sensor_board_rev0/Core/Src/main.c
+++ sw/sensor_board/sensor_board_rev0/Core/Src/main.c
@@ -19,6 +19,7 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "can.h"
+#include "dma.h"
 #include "spi.h"
 #include "tim.h"
 #include "usart.h"
@@ -93,6 +94,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_DMA_Init();
   MX_CAN_Init();
   MX_SPI1_Init();
   MX_USART1_UART_Init();
--- sw/sensor_board/sensor_board_rev0/Core/Src/stm32f0xx_it.c
+++ sw/sensor_board/sensor_board_rev0/Core/Src/stm32f0xx_it.c
@@ -57,6 +57,7 @@
 /* External variables --------------------------------------------------------*/
 extern TIM_HandleTypeDef htim16;
 extern TIM_HandleTypeDef htim17;
+extern DMA_HandleTypeDef hdma_usart1_rx;
 extern TIM_HandleTypeDef htim14;
 
 /* USER CODE BEGIN EV */
@@ -142,6 +143,20 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32f0xx.s).                    */
 /******************************************************************************/
+
+/**
+  * @brief This function handles DMA1 channel 2 and 3 interrupts.
+  */
+void DMA1_Channel2_3_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA1_Channel2_3_IRQn 0 */
+
+  /* USER CODE END DMA1_Channel2_3_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_usart1_rx);
+  /* USER CODE BEGIN DMA1_Channel2_3_IRQn 1 */
+
+  /* USER CODE END DMA1_Channel2_3_IRQn 1 */
+}
 
 /**
   * @brief This function handles TIM14 global interrupt.
