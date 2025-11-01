7ed2a8a4cb171617b4b61f87faf1579e23675aab
Anthony Luo <anthonyluo100@gmail.com>
2025-10-31 19:04:32 -0700

    SPI reading now in a separate function

--- sw/sensor_board/sensor_board_rev1_test/src/asm330.rs
+++ sw/sensor_board/sensor_board_rev1_test/src/asm330.rs
@@ -1,5 +1,12 @@
+use embedded_hal::spi::SpiBus;
 use log::info;
 
-pub fn check_who_am_i() {
-    info!("In the future I will be checking the ASM330 who_am_i");
+pub fn check_who_am_i<S>(spi: &mut S) -> Result<u8, S::Error>
+where
+    S: SpiBus<u8>,
+    S::Error: core::fmt::Debug,
+{
+    let mut buf: [u8; 2] = [0x8F, 0x00];
+    spi.transfer_in_place(&mut buf)?;
+    Ok(buf[1])
 }
