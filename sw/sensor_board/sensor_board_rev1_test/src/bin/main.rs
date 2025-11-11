e361354d05f7f8175401ea097c321c020ad78a9a
Anthony Luo <anthonyluo100@gmail.com>
2025-11-10 17:51:43 -0800

    remove old code from main

--- sw/sensor_board/sensor_board_rev1_test/src/bin/main.rs
+++ sw/sensor_board/sensor_board_rev1_test/src/bin/main.rs
@@ -64,73 +64,10 @@ fn main() -> ! {
 
     let mut imu_spi = imu_spi_maybe.expect("Spi must be initialized to continue!");
 
-    let fsr_a = AccelFs::G2;
-    let odr_a = Odr::Hz104;
-    let _ = old_asm330::set_xl_fsr(&mut imu_spi, &fsr_a); // hiding the warnings for now
-    let _ = old_asm330::set_xl_odr(&mut imu_spi, odr_a);
-    info!("Hello world!");
-    user_led.toggle();
-    match old_asm330::check_who_am_i(&mut imu_spi) {
-        Ok(val) => {
-            info!("WhoAmI value is 0x{:02X}", val);
-        }
-        Err(e) => {
-            info!("SPI Error: {:?}", e);
-        }
-    }
-
     read_who_am_i(&mut imu_spi);
 
     loop {
-        match old_asm330::read_xl_xyz(&mut imu_spi) {
-            Ok(xl_raw_data) => {
-                debug!(
-                    "Got XL X: {}, Y: {}, Z: {}",
-                    xl_raw_data.x, xl_raw_data.y, xl_raw_data.z
-                );
-                let accel_x_g = old_asm330::fs_a_to_g(xl_raw_data.x, &fsr_a);
-                let accel_y_g = old_asm330::fs_a_to_g(xl_raw_data.y, &fsr_a);
-                let accel_z_g = old_asm330::fs_a_to_g(xl_raw_data.z, &fsr_a);
-
-                info!(
-                    "Accel: X={:.3}g Y={:.3}g Z={:.3}g",
-                    accel_x_g, accel_y_g, accel_z_g
-                );
-            }
-            Err(e) => {
-                warn!("Failed to read XL XYZ: {:?}", e);
-            }
-        }
-
-        // let g_z = match asm330::read_xl_z(&mut imu_spi) {
-        //     Ok(raw) => {
-        //         let g_val = asm330::fs_a_to_g(raw, &fsr_a);
-        //         info!("Got G_Z as {}g's", g_val);
-        //         Some(g_val)
-        //     }
-        //     Err(e) => None,
-        // };
-        // if let Some(val) = g_z {
-        //     info!("Unpacked g_z as {}g", val);
-        // } else {
-        //     warn!("No g_z value available");
-        // }
-
-        // let mut buffer: [u8; 2] = [0x8F, 0x00];
-        // match imu_spi.transfer(&mut buffer) {
-        //     Ok(_) => {
-        //         let whoami_value = buffer[1];
-        //
-        //         info!("whoami value is {}", whoami_value);
-        //     }
-        //     Err(e) => {
-        //         info!("SPI Error: {:?}", e);
-        //     }
-        // }
-
         let delay_start = Instant::now();
         while delay_start.elapsed() < Duration::from_millis(10) {}
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-rc.1/examples/src/bin
 }
